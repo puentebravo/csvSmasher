f4cc4318f394cfb251170af7285989ea91c042af
Puentebravo <puentematos@gmail.com>
2023-11-20 17:06:36 -0500

    Modularized code, added os/getenv functions. Implemented Merge

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import pandas as pd;
+from dotenv import load_dotenv;
+import os;
+load_dotenv();
 
-mainData = pd.read_csv("./data/contacts.csv")
-memberData = pd.read_csv("./data/members.csv")
-subData = pd.read_csv("./data/subscriptions.csv")
+mainData = pd.read_csv(os.getenv("MAIN_DOC_PATH"))
+subData = pd.read_csv(os.getenv("SUB_DOC_PATH"))
+primaryKey = os.getenv("PRIMARY_KEY")
 
 
+newData = pd.merge(mainData, subData, on=primaryKey, how="left", left_on=None, right_on=None, left_index=False, right_index=False, sort=False, copy=True)
+
+
+
+newData.to_csv("./dist/combined.csv")
